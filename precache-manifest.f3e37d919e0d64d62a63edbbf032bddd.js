61f7749b7e383892eb14bbe251ba81156e05d75f
EthanB <ebonsignori@gmail.com>
2019-07-02 15:42:48 -0400

    Updates

---
+++ precache-manifest.f3e37d919e0d64d62a63edbbf032bddd.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bcf97ac9708796835f65ec22ed68592",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "7978374fcc0327e28379",
+    "url": "/Memory-Game/static/css/main.27ad1e30.chunk.css"
+  },
+  {
+    "revision": "e89bac3b237023793815",
+    "url": "/Memory-Game/static/js/2.0db4d231.chunk.js"
+  },
+  {
+    "revision": "7978374fcc0327e28379",
+    "url": "/Memory-Game/static/js/main.c5dce907.chunk.js"
+  },
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  },
+  {
+    "revision": "5099b7710fa5b66459a7bca04f494250",
+    "url": "/Memory-Game/static/media/Aragorn1_lg.5099b771.png"
+  },
+  {
+    "revision": "66fe62a058e44c4209c71f5fd59ebb48",
+    "url": "/Memory-Game/static/media/Bilbo1_lg.66fe62a0.png"
+  },
+  {
+    "revision": "817171566b5c4789801ee003790ad0e5",
+    "url": "/Memory-Game/static/media/Bombur.81717156.png"
+  },
+  {
+    "revision": "d7cac610b98e3c0541e094a7fdafc9e2",
+    "url": "/Memory-Game/static/media/Bombur1_lg.d7cac610.png"
+  },
+  {
+    "revision": "47e82d5fdddb3599025f3ba6a81798a1",
+    "url": "/Memory-Game/static/media/Boromir.47e82d5f.png"
+  },
+  {
+    "revision": "c6464cb7e1299a575f770eae3e3aa5fe",
+    "url": "/Memory-Game/static/media/Boromir1_lg.c6464cb7.png"
+  },
+  {
+    "revision": "1f10bc669c30058a5d6196f48e0aa840",
+    "url": "/Memory-Game/static/media/FirstOrder.1f10bc66.ttf"
+  },
+  {
+    "revision": "0b42b060dc4f6299379d1d293df891f3",
+    "url": "/Memory-Game/static/media/Frodo1_lg.0b42b060.png"
+  },
+  {
+    "revision": "22fe1a87904b3d5e811c502d2434c06b",
+    "url": "/Memory-Game/static/media/Gandalf2_lg.22fe1a87.png"
+  },
+  {
+    "revision": "b3c6a1550e9b1d24e22762419cd8c430",
+    "url": "/Memory-Game/static/media/Gimli1_lg.b3c6a155.png"
+  },
+  {
+    "revision": "f70a9dd9fd687671944b4fd0c97e2765",
+    "url": "/Memory-Game/static/media/Legolas1_lg.f70a9dd9.png"
+  },
+  {
+    "revision": "b1ab2f9731d4c3de10df36ae5f37294f",
+    "url": "/Memory-Game/static/media/Merry1_lg.b1ab2f97.png"
+  },
+  {
+    "revision": "0e6bb20b30c09569defa9db269dbb573",
+    "url": "/Memory-Game/static/media/Pippin.0e6bb20b.png"
+  },
+  {
+    "revision": "fd6968fcb1960312981b66cb13a72b20",
+    "url": "/Memory-Game/static/media/Pippin1_lg.fd6968fc.png"
+  },
+  {
+    "revision": "e76ce2c865dec12f0781931497a48bd9",
+    "url": "/Memory-Game/static/media/Samwise1_lg.e76ce2c8.png"
+  },
+  {
+    "revision": "14d4c317136d0739574447bc3f179805",
+    "url": "/Memory-Game/static/media/TengwarCursive.14d4c317.woff"
+  },
+  {
+    "revision": "8a0a68e1e9ad315dbc72771375ca5da3",
+    "url": "/Memory-Game/static/media/TengwarCursive.8a0a68e1.woff2"
+  },
+  {
+    "revision": "b0fd98f589f8e0209eeb94bf726173d7",
+    "url": "/Memory-Game/static/media/Thorin.b0fd98f5.png"
+  },
+  {
+    "revision": "2bbb25a9f034fa97b063a4f3030d8b7e",
+    "url": "/Memory-Game/static/media/Thorin1_lg.2bbb25a9.png"
+  },
+  {
+    "revision": "dc7e29b2aa913febee33536aa5b3b3d1",
+    "url": "/Memory-Game/static/media/lotr_bg.dc7e29b2.jpg"
+  },
+  {
+    "revision": "c13d8d57264d350d40c03c3f05f0484e",
+    "url": "/Memory-Game/static/media/ringm___-webfont.c13d8d57.woff"
+  },
+  {
+    "revision": "ebb81d0eb7b5dd1ed5444c7be10f80b7",
+    "url": "/Memory-Game/static/media/ringm___-webfont.ebb81d0e.woff2"
+  }
+]);
